724fd60a53b932f1a81d3432f00274bc85367423
Semih Gökduman <semihgokduman@gmail.com>
2020-03-16 13:25:39 +0100

    dist

---
+++ dist/3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{4:function(n,t,e){"use strict";e.r(t),e.d(t,"bootstrap",(function(){return d})),e.d(t,"mount",(function(){return p})),e.d(t,"unmount",(function(){return f}));var o=e(5),r=e.n(o),u=e(14),c=e.n(u),a=e(18),m=function(){return r.a.createElement("h1",null,"Hello from React")};var i=e.n(a)()({React:r.a,ReactDOM:c.a,rootComponent:m,domElementGetter:function(){return document.getElementById("react")}}),d=[i.bootstrap],p=[i.mount],f=[i.unmount]}}]);
+//# sourceMappingURL=3.js.map
